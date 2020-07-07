8557a48d5545e3c3b004349e44ee79198ee64471
Aoi <blue89azul@gmail.com>
2020-07-07 08:02:00 +0000

    タスク整理

---
+++ task09/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    // 課題４
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
+
+// 課題１
+// ルーティング
+
+// 課題２
+// コード記述及び変更の際に、記述量が少なくなる。
+
+// 課題３
+// Route::get('XXX', 'AAAController@bbb');
+
+// 課題４
+// 上記に記載。
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
---
+++ task10/profile/edit.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta hettp-equiv="X-UA-compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>My News</title>
+    </head>
+    <body>
+        <h1>プロフィール編集画面</h1>
+    </body>
+</html>
