25e088191f7245e81959ee02178a893126bf3528
Aoi <blue89azul@gmail.com>
2020-07-30 11:55:10 +0000

    課題再提出

--- task03.php
+++ task03.php
@@ -28,7 +28,7 @@ foreach ($fruits as $fruit) {
 $start = 1;
 $end = 100;
 for ($i = $start; $i <= $end; $i++) {
-    if ($i!==0 && $i%5 === 0) {
+    if ($i%5 === 0) {
         echo $i;
         echo "\n";
     }
